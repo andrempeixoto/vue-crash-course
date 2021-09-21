31497767d261f69f09ef197ebdd47e2b64e5d34a
Andre Peixoto <peixoto.amp@gmail.com>
2021-09-21 19:46:14 -0300

    Initial setup: Create Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped lang="css">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1.25 rem;
+}
+</style>
